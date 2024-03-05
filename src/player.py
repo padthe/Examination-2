aca42b3a4b45a843de823237679cd7b4041b8f35
padthe <n.patrick040@gmail.com>
2024-03-05 19:52:25 +0100

    Refactor game logic and player class

--- src/player.py
+++ src/player.py
@@ -5,16 +5,16 @@ from hand import Hand
 class Player:
     def __init__(self):
         self.hand = Hand()
-        self.username = self.set_username()
 
-    def set_username(self):
-        print("Enter your username:")
-        return input()
-
-    def change_username(self):
-        print("Enter your new username:")
-        self.username = input()
+    def user_name(self):
+        user_name = input()
+        return user_name
+    
+    def change_user_name(self):
+        user_name = input()
+        return user_name
 
     def draw_card(self, deck):
         card = deck.deal_card()
         self.hand.add_card(card)
+
