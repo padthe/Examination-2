14527e626a54d61ce0f75f6887ddd470f61bad70
padthe <n.patrick040@gmail.com>
2024-03-06 19:09:14 +0100

    Update main menu and player name handling

--- src/main.py
+++ src/main.py
@@ -16,8 +16,9 @@ def main_menu():
             print("1. Play")
             print("2. Check Scoreboard")
             print("3. See Rules")
-            print("4. Credits")
-            print("5. Exit")
+            print("4. Settings")
+            print("5. Credits")
+            print("6. Exit")
             choice = input("Enter your choice: ")
 
             if choice == "1":
@@ -28,8 +29,14 @@ def main_menu():
             elif choice == "3":
                 Game.game_rules()
             elif choice == "4":
-                Game.game_credits()
+                old_name = player.user_name
+                player.change_user_name()
+                new_name = player.user_name
+                scoreboard.update_user_name(old_name, new_name)
+                scoreboard.save_scores()
             elif choice == "5":
+                Game.game_credits()
+            elif choice == "6":
                 print("Exiting the game. Goodbye!")
                 break
             else:
